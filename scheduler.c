417d8f8765cc535e45e9d50fa5eb224dad1d9d95
edarsh7 <edarsh7@gmail.com>
2021-02-20 21:43:56 -0800

    ok

--- scheduler.c
+++ scheduler.c
@@ -1,13 +1,20 @@
-/**
+ /**
  * See scheduler.h for function details. All are callbacks; i.e. the simulator 
  * calls you when something interesting happens.
  */
 #include <stdlib.h>
-
+#include <stdio.h>
 #include "simulator.h"
 #include "scheduler.h"
 
-void scheduler(enum algorithm algorithm, unsigned int quantum) { }
+struct node {
+    thread_t * thread;
+    struct node* next;
+    struct node* prev;
+}node;
+
+void scheduler(enum algorithm algorithm, unsigned int quantum) 
+{ }
 
 void sim_tick() { }
 
@@ -26,6 +33,44 @@ void io_complete(thread_t *t) { }
 void io_starting(thread_t *t) { }
 
 stats_t *stats() { 
-  int i = 0; // Remove - only here to fail code warnings check
-  return NULL; 
+  
 }
+
+
+
+// -------------------- list functions from geeksforgeeks -------------
+
+void append(node** head_ref, struct_t * t) 
+{ 
+    /* 1. allocate node */
+    node* new_node = new node(); 
+ 
+    node* last = *head_ref; /* used in step 5*/
+
+    new_node->thread = t;
+
+    /* 3. This new node is going to be the last node, so 
+        make next of it as NULL*/
+    new_node->next = NULL; 
+ 
+    /* 4. If the Linked List is empty, then make the new 
+        node as head */
+    if (*head_ref == NULL)
+    { 
+        new_node->prev = NULL; 
+        *head_ref = new_node; 
+        return; 
+    } 
+ 
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL) 
+        last = last->next; 
+ 
+    /* 6. Change the next of last node */
+    last->next = new_node; 
+ 
+    /* 7. Make last node as previous of new node */
+    new_node->prev = last; 
+ 
+    return; 
+} 
