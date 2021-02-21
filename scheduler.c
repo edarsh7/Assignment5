df859d4b4737e04d0c1c4d69f2c35093ee4b0324
edarsh7 <edarsh7@gmail.com>
2021-02-20 21:47:52 -0800

    ok

--- scheduler.c
+++ scheduler.c
@@ -42,37 +42,36 @@ stats_t *stats() {
 
 // -------------------- list functions from geeksforgeeks -------------
 
-void append(node** head_ref, thread_t * t) 
-{ 
+void append(struct node** head_ref, thread_t t)
+{
     /* 1. allocate node */
-    node* new_node = new node(); 
+    struct node* new_node
+        = (struct node*)malloc(sizeof(struct node));
  
-    node* last = *head_ref; /* used in step 5*/
-
+    struct node* last = *head_ref; /* used in step 5*/
+ 
+    /* 3. This new node is going to be the last node, so
+          make next of it as NULL*/
+    new_node->next = NULL;
     new_node->thread = t;
-
-    /* 3. This new node is going to be the last node, so 
-        make next of it as NULL*/
-    new_node->next = NULL; 
  
-    /* 4. If the Linked List is empty, then make the new 
-        node as head */
-    if (*head_ref == NULL)
-    { 
-        new_node->prev = NULL; 
-        *head_ref = new_node; 
-        return; 
-    } 
+    /* 4. If the Linked List is empty, then make the new
+          node as head */
+    if (*head_ref == NULL) {
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
  
     /* 5. Else traverse till the last node */
-    while (last->next != NULL) 
-        last = last->next; 
+    while (last->next != NULL)
+        last = last->next;
  
     /* 6. Change the next of last node */
-    last->next = new_node; 
+    last->next = new_node;
  
     /* 7. Make last node as previous of new node */
-    new_node->prev = last; 
+    new_node->prev = last;
  
-    return; 
-} 
+    return;
+}
