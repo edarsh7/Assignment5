a3ba27ff9bc8e813d4794ff21894c072f8e2356e
edarsh7 <edarsh7@gmail.com>
2021-02-20 21:50:49 -0800

    ok

--- scheduler.c
+++ scheduler.c
@@ -13,7 +13,7 @@ struct node {
     struct node* prev;
 }node;
 
-void append(node** head_ref, thread_t * t);
+void append(struct node** head_ref, thread_t * t);
 
 void scheduler(enum algorithm algorithm, unsigned int quantum) 
 { }
@@ -34,44 +34,41 @@ void io_complete(thread_t *t) { }
 
 void io_starting(thread_t *t) { }
 
-stats_t *stats() { 
-  
-}
+stats_t *stats() { }
 
 
 
-// -------------------- list functions from geeksforgeeks -------------
 
 void append(struct node** head_ref, thread_t * t)
 {
-    /* 1. allocate node */
-    struct node* new_node
-        = (struct node*)malloc(sizeof(struct node));
- 
-    struct node* last = *head_ref; /* used in step 5*/
- 
-    /* 3. This new node is going to be the last node, so
-          make next of it as NULL*/
-    new_node->next = NULL;
-    new_node->thread = t;
- 
-    /* 4. If the Linked List is empty, then make the new
-          node as head */
-    if (*head_ref == NULL) {
-        new_node->prev = NULL;
-        *head_ref = new_node;
-        return;
-    }
- 
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-        last = last->next;
- 
-    /* 6. Change the next of last node */
-    last->next = new_node;
- 
-    /* 7. Make last node as previous of new node */
-    new_node->prev = last;
- 
-    return;
+  /* 1. allocate node */
+  struct node* new_node
+      = (struct node*)malloc(sizeof(struct node));
+
+  struct node* last = *head_ref; /* used in step 5*/
+
+  /* 3. This new node is going to be the last node, so
+        make next of it as NULL*/
+  new_node->next = NULL;
+  new_node->thread = t;
+
+  /* 4. If the Linked List is empty, then make the new
+        node as head */
+  if (*head_ref == NULL) {
+      new_node->prev = NULL;
+      *head_ref = new_node;
+      return;
+  }
+
+  /* 5. Else traverse till the last node */
+  while (last->next != NULL)
+      last = last->next;
+
+  /* 6. Change the next of last node */
+  last->next = new_node;
+
+  /* 7. Make last node as previous of new node */
+  new_node->prev = last;
+
+  return;
 }
