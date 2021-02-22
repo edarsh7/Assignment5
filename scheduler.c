43a1fd71357d16101090fa5e65fcd4bda45bce17
edarsh7 <edarsh7@gmail.com>
2021-02-22 15:16:14 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -37,7 +37,7 @@ struct node *thread_list = NULL;
 void append(struct node** head_ref, thread_t * t);
 void pop(struct node** head_ref);
 
-// ROUND ROBIN SET OF FUNCTIONS FOR SWITCH STATEMENT
+// ROUND ROBIN SET OF FUNCTIONS
 void rr_sysready();
 void rr_sysexec(thread_t *t);
 void rr_sys_rd_wr(thread_t *t);
@@ -45,6 +45,14 @@ void rr_sysexit(thread_t *t);
 void rr_iocomplete(thread_t *t);
 void turnaround(thread_t *td);
 void rr_iostarting(thread_t *t);
+
+//NON-PREEMPTIVE PRIORITY SET OF FUNCTIONS
+void np_prio_sysready();
+void np_prio_sysexec(thread_t *t);
+void np_prio_sys_rd_wr(thread_t *t);
+void np_prio_sysexit(thread_t *t);
+void np_prio_iocomplete(thread_t *t);
+void np_prio_iostarting(thread_t *t);
 
 
 void scheduler(enum algorithm algorithm, unsigned int quantum) 
@@ -60,6 +68,8 @@ void sim_ready()
   switch(algo_number){
     case ROUND_ROBIN:
       rr_sysready();
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_sysready();
     default:
       break;
   }
@@ -71,6 +81,8 @@ void sys_exec(thread_t *t)
   switch(algo_number){
     case ROUND_ROBIN:
       rr_sysexec(t);
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_sysexec(t);
     default:
       break;
   }
@@ -81,6 +93,8 @@ void sys_read(thread_t *t)
   switch(algo_number){
     case ROUND_ROBIN:
       rr_sys_rd_wr(t);
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_sys_rd_wr(t);
     default:
       break;
   }
@@ -91,6 +105,8 @@ void sys_write(thread_t *t)
   switch(algo_number){
     case ROUND_ROBIN:
       rr_sys_rd_wr(t);
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_sys_rd_wr(t);
     default:
       break;
   }
@@ -101,6 +117,8 @@ void sys_exit(thread_t *t)
   switch(algo_number){
     case ROUND_ROBIN:
       rr_sysexit(t);
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_sysexit(t);
     default:
       break;
   }
@@ -111,6 +129,8 @@ void io_complete(thread_t *t)
   switch(algo_number){
     case ROUND_ROBIN:
       rr_iocomplete(t);
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_iocomplete(t);
     default:
       break;
   }
@@ -121,6 +141,8 @@ void io_starting(thread_t *t)
   switch(algo_number){
     case ROUND_ROBIN:
       rr_iostarting(t);
+    case NON_PREEMPTIVE_PRIORITY:
+      np_prio_iostarting(t);
     default:
       break;
   }
@@ -163,37 +185,6 @@ stats_t *stats()
 
 /*= = = = = = = = = = = = = = = = = ROUND ROBIN FUNCTIONS = = = = = = = = = = = = = = = = =*/
 
-void append(struct node** head_ref, thread_t * t)
-{
-  /* 1. allocate node */
-  struct node* new_node
-      = (struct node*)malloc(sizeof(struct node));
-
-  struct node* last = *head_ref; /* used in step 5*/
-
-  /* 3. This new node is going to be the last node, so
-        make next of it as NULL*/
-  new_node->next = NULL;
-  new_node->thread = t;
-  new_node->quantum_ct = q_value;
-  new_node->waittime = 0;
-  new_node->turnaround = 0;
-  /* 4. If the Linked List is empty, then make the new
-        node as head */
-  if (*head_ref == NULL) {
-      *head_ref = new_node;
-      return;
-  }
-
-  /* 5. Else traverse till the last node */
-  while (last->next != NULL)
-      last = last->next;
-
-  /* 6. Change the next of last node */
-  last->next = new_node;
-
-  return;
-}
 
 void pop(struct node** head_ref)
 {
@@ -357,7 +348,7 @@ void rr_iocomplete(thread_t *t)
   io_thread = NULL;
 }
 
-
+//IOSTARTING implementation for IOSTARTING
 void rr_iostarting(thread_t *t)
 {
   struct node *temp;
@@ -385,6 +376,135 @@ void rr_iostarting(thread_t *t)
 }
 /*= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =*/
 
+void np_prio_sysready()
+{
+  
+}
+
+void np_prio_sysexec(thread_t *t)
+{
+  count++;
+  
+  append(&thread_list, t);
+  sortedInsert(&head, t);
+
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread->tid != t->tid)
+  {
+    temp = temp->next;
+  }
+  temp->arrival = sim_time();
+  temp->waittime = 0;
+  temp->ready_q = 1;
+  temp->done = 0;
+  
+  if(head != NULL)
+  {
+    sim_dispatch(head->thread);
+    running_thread = head->thread;
+  }
+
+  if(running_thread == temp->thread)
+  {
+    temp->ready_q = 0;
+  }
+  
+}
+
+void np_prio_sys_rd_wr(thread_t *t)
+{
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread->tid != t->tid)
+  {
+    temp = temp->next;
+  }
+  temp->ready_q = 0;
+  temp->io_wait = sim_time();
+
+  pop(&head);
+  if(head != NULL)
+  {
+    sim_dispatch(head->thread);
+    running_thread = head->thread;
+  }
+}
+
+void np_prio_sysexit(thread_t *t)
+{
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread->tid != t->tid)
+  {
+    temp = temp->next;
+  }
+  temp->completion = sim_time();
+  temp->ready_q = 0;
+  temp->done = 1;
+
+  pop(&head);
+  if(head != NULL)
+  {
+    sim_dispatch(head->thread);
+    running_thread = head->thread;
+  }
+  if(running_thread == temp->thread)
+  {
+    temp->ready_q = 0;
+  }
+}
+
+void np_prio_iocomplete(thread_t *t)
+{
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread->tid != t->tid)
+  {
+    temp = temp->next;
+  }
+  temp->ready_q = 1;
+
+  append(&head, t);
+  if(head != NULL)
+  {
+    sim_dispatch(head->thread);
+    running_thread = head->thread;
+  }
+  if(running_thread == temp->thread)
+  {
+    temp->ready_q = 0;
+  }
+  io_thread = NULL;
+}
+
+void np_prio_iostarting(thread_t *t)
+{
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread->tid != t->tid)
+  {
+    temp = temp->next;
+  }
+  temp->ready_q = 0;
+  temp->io_start = sim_time();
+
+  temp->waittime = temp->waittime + (temp->io_start - temp->io_wait - 1);
+  temp->io_wait = 0;
+  temp->io_start = 0;
+
+  if(head != NULL)
+  {
+    sim_dispatch(head->thread);
+    running_thread = head->thread;
+  }
+  if(running_thread == temp->thread)
+  {
+    temp->ready_q = 0;
+  }
+}
+
+
 void turnaround(thread_t *td)
 {
   struct node *temp;
@@ -396,4 +516,61 @@ void turnaround(thread_t *td)
   }
 
   temp->turnaround = temp->completion - temp->arrival + 1;
+}
+
+void sortedInsert(struct Node** head_ref, thread_t *t) 
+{ 
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->next = NULL;
+    new_node->thread = t;
+
+    struct node *temp;
+
+    if (*head_ref == NULL) 
+    { 
+      new_node->next = *head_ref; 
+      *head_ref = new_node; 
+    } 
+    else
+    { 
+      temp = *head_ref; 
+      while (temp->next != NULL && temp->next->thread->priority <= new_node->thread->priority) 
+      { 
+        temp = temp->next; 
+      } 
+      new_node->next = temp->next; 
+      temp->next = new_node; 
+    } 
+} 
+
+void append(struct node** head_ref, thread_t * t)
+{
+  /* 1. allocate node */
+  struct node* new_node
+      = (struct node*)malloc(sizeof(struct node));
+
+  struct node* last = *head_ref; /* used in step 5*/
+
+  /* 3. This new node is going to be the last node, so
+        make next of it as NULL*/
+  new_node->next = NULL;
+  new_node->thread = t;
+  new_node->quantum_ct = q_value;
+  new_node->waittime = 0;
+  new_node->turnaround = 0;
+  /* 4. If the Linked List is empty, then make the new
+        node as head */
+  if (*head_ref == NULL) {
+      *head_ref = new_node;
+      return;
+  }
+
+  /* 5. Else traverse till the last node */
+  while (last->next != NULL)
+      last = last->next;
+
+  /* 6. Change the next of last node */
+  last->next = new_node;
+
+  return;
 }
