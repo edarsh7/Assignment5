1fac508cfe7c0fd25cff353759ea1250af6fc0fe
edarsh7 <edarsh7@gmail.com>
2021-02-21 21:16:43 -0800

    ok

--- scheduler.c
+++ scheduler.c
@@ -50,7 +50,7 @@ void sim_ready()
     {
       head->quantum_ct = q_value;
       pop(&head);
-      append(&temp->thread);
+      append(&temp);
       sim_dispatch(head->thread);
     }
     temp->quantum_ct--;
