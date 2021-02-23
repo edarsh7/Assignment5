5396f4d5fa25818ee2be503dc783b1fa5608779c
edarsh7 <edarsh7@gmail.com>
2021-02-22 17:40:02 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -67,72 +67,86 @@ void sim_tick() { }
 
 void sim_ready() 
 {
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_sysready();
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_sysready();
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_sysready();
   }
 }
 
 void sys_exec(thread_t *t) 
 {
   count++;
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_sysexec(t);
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_sysexec(t);
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_sysexec(t);
   }
 }
 
 void sys_read(thread_t *t) 
 { 
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_sys_rd_wr(t);
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_sys_rd_wr(t);
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_sys_rd_wr(t);
   }
 }
 
 void sys_write(thread_t *t) 
 {
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_sys_rd_wr(t);
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_sys_rd_wr(t);
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_sys_rd_wr(t);
   }
 }
 
 void sys_exit(thread_t *t) 
 { 
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_sysexit(t);
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_sysexit(t);
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_sysexit(t);
   }
 }
 
 void io_complete(thread_t *t) 
 { 
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_iocomplete(t);
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_iocomplete(t);
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_iocomplete(t);
   }
 }
 
 void io_starting(thread_t *t)
 {
-  switch(algo_number){
-    case ROUND_ROBIN:
-      rr_iostarting(t);
-    default:
-      break;
+  if(algo_number == ROUND_ROBIN)
+  {
+    rr_iostarting(t);
+  }
+  else if(algo_number == NON_PREEMPTIVE_PRIORITY)
+  {
+    np_prio_iostarting(t);
   }
 }
 
