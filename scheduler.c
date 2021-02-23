655ced53b166a059305017731bf4cfe55a30f814
edarsh7 <edarsh7@gmail.com>
2021-02-22 22:53:53 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -569,7 +569,7 @@ void sortedInsert(struct node** head_ref, thread_t *t)
     else
     { 
       temp = (*head_ref); 
-      while (temp->next != NULL && temp->thread->priority <= new_node->thread->priority) 
+      while (temp->next != NULL && temp->next->thread->priority <= new_node->thread->priority) 
       { 
         temp = temp->next; 
       } 
