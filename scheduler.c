45684a555c937a3b35f1860cb773f7ba9b4806b9
edarsh7 <edarsh7@gmail.com>
2021-02-22 20:51:31 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -496,6 +496,15 @@ void np_prio_iostarting(thread_t *t)
 /*= = = = = = = = = = = = = = = = = NP_PRIO FUNCTIONS = = = = = = = = = = = = = = = = =*/
 void prmtv_prio_sysready()
 {
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread != head->thread)
+  {
+    temp = temp->next;
+  }
+  if(head->thread == temp->thread && temp->done == 1)
+    pop(&head);
+
   if(head != NULL)
   {
     thread_t *temp;
@@ -523,7 +532,15 @@ void prmtv_prio_sys_rd_wr(thread_t *t)
 void prmtv_prio_sysexit(thread_t *t)
 {
   running_thread = NULL;
-  pop(&head);
+
+  struct node *temp;
+  temp = thread_list;
+  while(temp->thread->tid != t->tid)
+  {
+    temp = temp->next;
+  }
+  temp->done = 1;
+
 }
 
 void prmtv_prio_iocomplete(thread_t *t)
